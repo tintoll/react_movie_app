51188cad37e83d5ae00d18c696eda3773bd63ed4
tintoll <belselios@gmail.com>
2020-05-23 23:11:57 +0900

    랜딩페이지 구성(데이터가져오기)

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '513b6127e96b699366d870fdca10698f';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React , {useEffect, useState} from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config'
+import MainImage from './Sections/MainImage'
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            setMovies([...data.results]);
+            setMainMovieImage(data.results[0]);
+        } );
+
+
+    }, [])
+
     return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width:'100%', margin :0}}>
+
+            {/* Main Image*/}
+            {
+                MainMovieImage &&
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
+                    title={MainMovieImage.origin_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+            
+
+            <div style={{ width:'85%', maring:'1rem auto'}}>
+                <h2>Movide by latest</h2>
+                <hr />
+
+                {/* Grid Card */}
+                 
+            </div>
+            <div style={{ display : 'flex', justifyContent : 'center'}}>
+                <button>load more</button>
+            </div>
+       
         </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
     )
 }
 
