176ff47b97f3badcd4ae0ba255369a8b07a30f42
tintoll <belselios@gmail.com>
2020-05-25 16:16:21 +0900

    즐겨찾기 추가/삭제 API 추가

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -8,6 +8,7 @@ function Favorite(props) {
     const movieRunTime = props.movieInfo.runtime;
 
     const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [FavoriteToggle, setFavoriteToggle] = useState(false)
     useEffect(() => {
         
         let variables = {
@@ -24,11 +25,58 @@ function Favorite(props) {
                 alert('즐겨찾기 정보 가져오기 실패')
             }
         })
+        Axios.post('/api/favorite/favorited', variables)
+        .then(response => {
+            if(response.data.success) {
+                console.log(response.data);
+                setFavoriteToggle(response.data.favorited);
+            }else {
+                alert('즐겨찾기 정보 가져오기 실패')
+            }
+        })
+
     }, [])
+
+    const handleOnclick = () => {
+        let variables = {
+            userFrom : props.userFrom,
+            movieId : props.movieId,
+            movieTitle,
+            moviePost,
+            movieRunTime
+        }
+
+        // 내가 누른 상태냐? 아니냐?
+        if(!FavoriteToggle) {
+            Axios.post('/api/favorite/add', variables)
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data);
+                    setFavoriteNumber(FavoriteNumber + 1)
+                    setFavoriteToggle(true)
+                }else {
+                    alert('즐겨찾기 추가 실패')
+                }
+            })
+        } else {
+            Axios.post('/api/favorite/delete', variables)
+            .then(response => {
+                if(response.data.success) {                    
+                    setFavoriteNumber(FavoriteNumber - 1)
+                    setFavoriteToggle(false)
+                }else {
+                    alert('즐겨찾기 삭제 실패')
+                }
+            })
+        }
+        
+    }
 
     return (
         <div>
-            <button>Favorite {FavoriteNumber}</button>
+            <button onClick={handleOnclick}>
+            {FavoriteToggle ? 'Favorited' : 'Not Favorited'} {FavoriteNumber}
+            </button>
         </div>
     )
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -16,4 +16,34 @@ router.post('/favoriteNumber', (req, res) => {
             res.status(200).json({success:true, favoriteNumber : favorites.length})
         })
 })
+router.post('/favorited', (req, res) => {
+    Favorite.find({'movieId':req.body.movieId, 'userFrom' : req.body.userFrom })
+        .exec((err, favorites) => {
+            if(err) return res.status(400).json({success:false, err})
+            let result = false;
+            if(favorites.length !== 0) {
+                result = true;
+            }
+            res.status(200).json({success:true, favorited: result});             
+        })
+})
+
+
+router.post('/add', (req, res) => {    
+    const favorite = new Favorite(req.body);
+    favorite.save((err, favorite) => {
+            if(err) return res.status(400).json({success:false, err})
+            res.status(200).json({success:true})
+        })
+})
+router.post('/delete', (req, res) => {    
+
+    Favorite.findOneAndDelete({'movieId':req.body.movieId, 'userFrom':req.body.userFrom })
+        .exec((err, favorite) => {
+            if(err) return res.status(400).json({success:false, err})
+
+            res.status(200).json({success:true})
+        })
+})
+
 module.exports = router;
